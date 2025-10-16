f6479a5db3950b2b4f1c25cf0cbce4cd64daecec
MerchPS <merchpsbackup@gmail.com>
2025-10-16 20:51:35 +0700

    Delete api directory

--- api/apiku.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://generich.my.id/apivote");
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Gagal mengambil data API" });
-  }
-}
